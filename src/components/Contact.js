ac0b62896e6bbced7676ab6a467203e71d89dd8f
krawczyk071 <krawczyk071@gmail.com>
2023-04-26 11:19:30 +0200

    contact form with emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,43 @@
-import React from "react";
-import { contact } from "../utils/data";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const [alert, setAlert] = useState({});
+  const [formData, setFormData] = useState({});
+  const form = useRef();
+
+  const formChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const { user_name, user_email, message } = formData;
+    if (!user_name || !user_email || !message) {
+      setAlert({ type: "error", txt: "Please provide value for each field" });
+
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_MAILJS_SERVICE_ID,
+        "template_myh855m",
+        form.current,
+        "2txkHWAn6NyflTMvH"
+      )
+      .then(
+        (result) => {
+          setAlert({ type: "success", txt: "Email sent. Thank you." });
+          // console.log(result);
+          // e.target.reset();
+          setFormData({ user_name: "", user_email: "", message: "" });
+        },
+        (error) => {
+          setAlert({ type: "error", txt: error.text });
+        }
+      );
+  };
   return (
     <div className="contact main" id="contact">
       <h1>CONTACT</h1>
@@ -9,25 +45,36 @@ const Contact = () => {
         Feel free to Contact me by submitting the form below and I will get back
         to you as soon as possible
       </p>
-      <form action="#" method="post" className="form">
+      <div
+        className={`contact__alert ${
+          alert.type === "error" ? "contact_alert__err" : "contact_alert__suc"
+        }`}
+      >
+        {alert && <>{alert.txt}</>}
+      </div>
+      <form ref={form} onSubmit={sendEmail} className="form">
         <div>
-          <label for="name">Name</label>
+          <label for="user_name">Name</label>
           <input
             placeholder="Enter Your Name"
             type="text"
-            name="name"
-            id="name"
+            name="user_name"
+            id="user_name"
+            onChange={formChange}
+            value={formData.user_name}
             className="ipt"
           />
         </div>
         <div>
-          <label for="email">Email</label>
+          <label for="user_email">Email</label>
           <input
             placeholder="Enter Your Email"
             type="email"
-            name="email"
-            id="email"
+            name="user_email"
+            id="user_email"
             className="ipt"
+            onChange={formChange}
+            value={formData.user_email}
           />
         </div>
         <div>
@@ -38,11 +85,13 @@ const Contact = () => {
             placeholder="Enter Your Message"
             name="message"
             id="message"
+            onChange={formChange}
+            value={formData.message}
             className="ipt"
           ></textarea>
         </div>
         <button type="submit" className="btn form__btn">
-          Submit
+          Send
         </button>
       </form>
       {/* <form on onSubmit={() => console.log("submited")}>
